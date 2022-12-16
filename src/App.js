2dacb6dd3a56ae62c083e0b9c2da2d358695367c
ManeeshChikoti <maneeshchikoti@gmail.com>
2022-12-16 17:59:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useContext} from "react";
+import { AuthContext } from "./AuthContext";
+import Home from "./home/Home";
+import Login from "./login/Login";
 function App() {
+  const {state}=useContext(AuthContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {state.isLoggedIn ?<Home/>:   <Login/>}
+ 
     </div>
   );
 }
